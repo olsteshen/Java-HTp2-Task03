08851684ead8c380ac2e47441dd257c80c482ceb
stesho01 <Listo4ek>
2021-12-11 23:09:02 +0200

    Task 03 Fix program

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle myRect = new Rectangle();
+        myRect.width = 40;
+        myRect.height = 50;
+        System.out.println("myRect's area is " + myRect.width* myRect.height);
+    }
+}
